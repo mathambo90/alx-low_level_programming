7b88bc602d4e04e2466757f610e7bee0d03c3689
mathambo90 <pkhumalo90@gmail.com>
2023-07-13 14:51:05 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry
+ * discription - Size is not grandeur
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s) \n", sizeof(char));
+	printf(Size of an int: %zu byte(s)\n", sizeof(int));
+	printf(Size of along int: %zu byte(s)\n"' sizeof(long int));		 printf(Size of long long int: %zu byte(s) \n", sizeof(long long int));
+	printf(Size of a float: %zu byte(s) \n", sizeof(float));
+
+	return (0)
+}}
