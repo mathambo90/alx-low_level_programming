78c3f00ec8827289b7b446e78c5fe220e801ce3c
mathambo90 <pkhumalo90@gmail.com>
2023-07-17 13:41:06 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+  *main - Entry level
+  *describtion - Alphabet
+  *Return: Always (0)
+  */
+
+int main(void)
+{	
+	char c;
+	char d;	
+
+	c = 'a';
+	d = 'A';
+	while 
+		(c <= 'z') {
+		putchar(c);
+		c++;
+		}
+	while
+		(d <= 'z') {
+			putchar(d);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
